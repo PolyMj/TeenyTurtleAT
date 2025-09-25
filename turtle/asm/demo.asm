a6dba44682807468d5657f6076ad0a8d3a2b5526
Noah Breedy <noahfamily373@gmail.com>
2025-09-24 21:15:07 -0400

    turtle: inital commit
    The TeenyAT turtle system will provide assembly
    writers the chance to draw interactive sketches
    on either a blank or user provided canvas

---
+++ turtle/asm/demo.asm
@@ -0,0 +1,17 @@
+.const TURTLE_X       0xD000
+.const TURTLE_Y       0xD001
+.const TURTLE_ANGLE   0xD002
+.const SET_X          0xD003
+.const SET_Y          0xD004
+.const PEN_DOWN       0xD010
+.const PEN_UP         0xD011
+.const PEN_COLOR      0xD012
+.const PEN_SIZE       0xD013
+.const GOTO_XY        0xE000
+.const FACE_XY        0xE001
+.const MOVE           0xE002
+.const DETECT         0xE003
+.const SET_ERASER     0xE004
+
+!main
+    jmp !main
