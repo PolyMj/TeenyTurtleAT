c8213d5c50373a8be6471dfb629b1befd56ee0a2
Noah Breedy <noahfamily373@gmail.com>
2025-10-16 22:43:11 -0400

    Added example assembly using the MOVE function

---
+++ turtle/asm/demo2.asm
@@ -0,0 +1,71 @@
+; TeenyAT Constants
+.const PORT_A_DIR                0x8000
+.const PORT_B_DIR                0x8001
+.const PORT_A                    0x8002
+.const PORT_B                    0x8003
+.const RAND                      0x8010
+.const RAND_BITS                 0x8011
+.const INTERRUPT_VECTOR_TABLE    0x8E00
+.const INTERRUPT_ENABLE_REGISTER 0x8E10
+.const CONTROL_STATUS_REGISTER   0x8EFF
+
+; Turtle Peripherals
+.const TURTLE_X       0xD000
+.const TURTLE_Y       0xD001
+.const TURTLE_ANGLE   0xD002
+.const SET_X          0xD003
+.const SET_Y          0xD004
+.const PEN_DOWN       0xD010
+.const PEN_UP         0xD011
+.const PEN_COLOR      0xD012
+.const PEN_SIZE       0xD013
+.const GOTO_XY        0xE000
+.const FACE_XY        0xE001
+.const MOVE           0xE002
+.const DETECT         0xE003
+.const SET_ERASER     0xE004
+
+; Setup interrupt callback by mapping it to external interrupt 8
+set rA, !set_new_target_XY
+set rB, 8
+str [ INTERRUPT_VECTOR_TABLE + rB ], rA
+
+; Enable external interrupt 8
+set rA, 0b00000001_00000000
+str [ INTERRUPT_ENABLE_REGISTER ], rA
+
+; Enable interrupts globally
+set rA, 0b000000000000000_1
+str [ CONTROL_STATUS_REGISTER ], rA
+
+set rA, 2
+str [ PEN_DOWN ], rZ
+!main
+    str [ MOVE ], rA
+    str [ TURTLE_ANGLE ], rB
+    lod rC, [ RAND_BITS ]
+    str [ PEN_COLOR ], rC
+    lod rC, [ RAND ]
+    mod rC, 0x622
+    add rB, 5
+    dly rC
+    jmp !main
+
+;-------------------------------------------------
+!set_new_target_XY
+    psh rA
+    lod rA, [ RAND ]
+    mod rA, 641
+    str [ SET_X ], rA
+    lod rA, [RAND]
+    mod rA, 501
+    str [ SET_Y ], rA
+    str [ PEN_UP ], rZ
+    lod rA, [ RAND_BITS ]
+    str [ PEN_COLOR ], rA
+    mod rA, 2
+    je !return
+    str [ PEN_DOWN ], rZ
+!return
+    pop rA
+    rti
