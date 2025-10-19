0f7ea83cd56d18139a9f86482427c7e79be7cc71
Mj <ballarmj@sunypoly.edu>
2025-10-19 00:53:21 -0400

    Not quite working, but the idea is there

---
+++ turtle/asm/wall_bounce.asm
@@ -0,0 +1,96 @@
+; TeenyAT Constants
+.const PORT_A_DIR                0x8000
+.const PORT_B_DIR                0x8001
+.const PORT_A                    0x8002
+.const PORT_B                    0x8003
+.const RAND                      0x8010
+.const RAND_BITS                 0x8011
+.const INTERRUPT_VECTOR_TABLE    0x8E00
+.const INTERRUPT_ENABLE_REGISTER 0x8E10
+.const CONTROL_STATUS_REGISTER   0x8EFF
+
+; Turtle Peripherals
+.const TURTLE_X       0xD000
+.const TURTLE_Y       0xD001
+.const TURTLE_ANGLE   0xD002
+.const SET_X          0xD003
+.const SET_Y          0xD004
+.const PEN_DOWN       0xD010
+.const PEN_UP         0xD011
+.const PEN_COLOR      0xD012
+.const PEN_SIZE       0xD013
+.const GOTO_XY        0xE000
+.const FACE_XY        0xE001
+.const MOVE           0xE002
+.const DETECT         0xE003
+.const SET_ERASER     0xE004
+.const GET_COLOR_CHANGE  0xE010
+.const NEXT_COLOR_CHANGE 0xE011
+.const STOP_MOVE         0xE012
+
+; Setup interrupt callback by mapping it to external interrupt 8
+set rA, !set_new_target_XY
+set rB, 8
+str [ INTERRUPT_VECTOR_TABLE + rB ], rA
+
+; Setup interrupt callback by mapping it to external interrupt 10
+set rA, !color_change_interrupt
+set rB, 10
+str [ INTERRUPT_VECTOR_TABLE + rB ], rA
+
+; Enable external interrupt 8
+set rA, 0b00000101_00000000
+str [ INTERRUPT_ENABLE_REGISTER ], rA
+
+; Enable interrupts globally
+set rA, 0b000000000000000_1
+str [ CONTROL_STATUS_REGISTER ], rA
+
+str [ GOTO_XY ], rZ
+!main
+    set rA, rZ
+    jmp !main
+
+;-------------------------------------------------
+!set_new_target_XY
+    psh rA
+    lod rA, [ RAND ]
+    mod rA, 640
+    str [ SET_X ], rA
+    lod rA, [ RAND ]
+    mod rA, 500
+    str [ SET_Y ], rA
+    mod rA, 2
+    je !pen_goes_up
+    lod rA, [ RAND_BITS ]
+    str [ PEN_COLOR ], rA
+    str [ PEN_DOWN ], rZ
+    jmp !return
+!pen_goes_up
+    str [ PEN_UP ], rZ
+!return
+    str [ GOTO_XY ], rZ
+    str [ FACE_XY ], rZ
+    pop rA
+    rti
+
+!color_change_interrupt
+    psh rA
+    psh rB
+!cc_loop
+    lod rA, [ NEXT_COLOR_CHANGE ]
+    cmp rA, rZ
+    je !end_cc_loop
+        lod rA, [ GET_COLOR_CHANGE ]
+        inc rA
+        cmp rA, rZ
+        je !cc_loop
+        lod rA, [ RAND ]
+        mod rA, 640
+        str [ SET_X ], rA
+        lod rA, [ RAND ]
+        mod rA, 500
+        str [ SET_Y ], rA
+        str [ GOTO_XY ], rZ
+    jmp !cc_loop
+!end_cc_loop
