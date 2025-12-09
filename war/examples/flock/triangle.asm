71d36ea4287c2c954f123f880644684ee44d6ea7
Mj <ballarmj@sunypoly.edu>
2025-12-08 21:41:05 -0500

    Slight cleanup of flock example

--- war/examples/flock/triangle.asm
+++ war/examples/flock/triangle.asm
@@ -2,15 +2,8 @@
 .const MOVE_BACKWARD   0xD001
 .const MOVE_LEFT       0xD002
 .const MOVE_RIGHT      0xD003
-.const DETECT_FORWARD  0xE000
-.const DETECT_BACKWARD 0xE001
 .const DETECT_LEFT     0xE002
 .const DETECT_RIGHT    0xE003
-.const CURRENT_HEALTH  0xF000
-.const ALVIE_UNITS     0xF001
-.const DEAD_UNITS      0xF002
-
-set rE, 0
 
 !main
     lod rA, [DETECT_LEFT]
